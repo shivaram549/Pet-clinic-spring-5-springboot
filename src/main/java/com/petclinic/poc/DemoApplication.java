2a6fc9dfaf20bf9466a9388ca22ec67538945489
shivaram549 <31216227+shivaram549@users.noreply.github.com>
2019-07-05 16:22:35 +0530

    Add files via upload

---
+++ src/main/java/com/petclinic/poc/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.petclinic.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
